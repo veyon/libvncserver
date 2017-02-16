54839f00b97ae47a412ae2e61928b2668d7bf761
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-02-16 20:33:49 +0100

    rfbproto.h: define rfbUltraVNC_MsLogonIIAuth

--- rfb/rfbproto.h
+++ rfb/rfbproto.h
@@ -290,6 +290,7 @@ typedef char rfbProtocolVersionMsg[13];	/* allow extra byte for null */
 #define rfbTLS 18
 #define rfbVeNCrypt 19
 #define rfbARD 30
+#define rfbUltraVNC_MsLogonIIAuth 0x71
 #define rfbMSLogon 0xfffffffa
 
 #define rfbVeNCryptPlain 256
