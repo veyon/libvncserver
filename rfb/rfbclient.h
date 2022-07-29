dcb5c21dba100d24a7696fc97a1bc913d0ebfc7a
Tobias Junghans <tobydox@veyon.io>
2022-07-29 12:52:25 +0200

    Revert "rfb/rfbclient: document new screen member"
    This reverts commit 99842d5e8bf6577ef431217b791974497a229e21.

--- rfb/rfbclient.h
+++ rfb/rfbclient.h
@@ -473,9 +473,6 @@ typedef struct _rfbClient {
 	MUTEX(tlsRwMutex);
 
 	rfbBool requestedResize;
-        /**
-         * Used for intended dimensions, rfbClient.width and rfbClient.height are used to manage the real framebuffer dimensions.
-	 */
 	rfbExtDesktopScreen screen;
 } rfbClient;
 
